5979c85e65088638dcc6fc0950908a610df88549
Sam Hammersley <sam.gonsalves@protonmail.com>
2024-01-13 14:51:23 +0000

    Add automatic most recent solution finder, this is probably a bad idea lol

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate cargo;
 extern crate const_format;
+extern crate regex;
 
 use std::env;
 use std::fs;
@@ -8,11 +9,12 @@ use std::path::PathBuf;
 use const_format::concatcp;
 
 use cargo::core::Workspace;
-use cargo::ops::run;
 use cargo::ops::CompileOptions;
 use cargo::ops::Packages;
+use cargo::ops::*;
 use cargo::util::command_prelude::CompileMode;
 use cargo::Config;
+use regex::Regex;
 
 const SESSION_ID_VAR_KEY: &str = "AOC_SESSION_ID";
 const USER_AGENT_VAR_KEY: &str = "AOC_USER_AGENT";
@@ -42,13 +44,46 @@ pub fn get_advent_of_code_input(mut cwd_path: PathBuf, day: u32) -> Result<Strin
     })
 }
 
+/// Searches for directories in the given directory whose names match the regex pattern:
+///     `day_(\d+)`
+///
+/// # Arguments
+///
+/// * `cwd_path` - A PathBuf to the directory to look in.
+///
+/// This doesn't verify anything about the directory, we currently just assume that it's a
+/// directory that contains a binary crate.
+fn find_last_day(cwd_path: PathBuf) -> Result<u32, anyhow::Error> {
+    let re = Regex::new(r"day_(\d+)").unwrap();
+
+    let last_entry = fs::read_dir(cwd_path)?
+        .filter_map(|entry_result| {
+            let entry = entry_result.ok()?;
+            if entry.file_type().ok()?.is_dir() {
+                let file_name = entry.file_name();
+                let file_name = file_name.to_str()?;
+                if let Some(target_match) = re.captures(file_name) {
+                    if let Ok(parsed_value) = target_match[1].parse::<u32>() {
+                        return Some(parsed_value);
+                    }
+                }
+            }
+            None
+        })
+        .last();
+    last_entry.ok_or(anyhow::anyhow!("Failed to find any solutions!"))
+}
+
 fn main() -> Result<(), anyhow::Error> {
-    let args: Vec<String> = env::args().collect();
-    assert!(args.len() == 2, "Day number is required as argument!");
-
     let cwd_path = env::current_dir()?;
+    let args: Vec<String> = env::args().collect();
 
-    let day_number = args[1].parse::<u32>()?;
+    // if there is an argument for the day to run, use that or find last day_x crate
+    let day_number = match args.len() {
+        x if x != 2 => find_last_day(cwd_path.join(SOLUTIONS_DIR_PATH))?,
+        _ => args[1].parse::<u32>()?,
+    };
+
     let day_directory = &format!("day_{day_number}/");
 
     let mut manifest_path = cwd_path.join(SOLUTIONS_DIR_PATH);
